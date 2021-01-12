21c932b746a48e7d53fed034c4f4d137e221b1bf
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2021-01-12 16:31:11 +0000

    Fixed JavaScript bug. Can now Create quiz, questions and answers as desired.

--- QuizManager.Web/wwwroot/js/site.js
+++ QuizManager.Web/wwwroot/js/site.js
@@ -1,24 +1,23 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
+﻿
 function check(el) {
-    var checkBoxes = document.getElementsByName("correct-answer-checkbox");
-    checkBoxes.forEach(function (item) {
-        if (item != el) {
-            console.log(item);
-            item.checked = false;
-        }
-    });
-}
-
-function MutExChkList(chk) {
-    var chkList = chk.parentNode.parentNode.parentNode;
-    var chks = chkList.getElementsByName("correct-answer-checkbox");
-    for (var i = 0; i < chks.length; i++) {
-        if (chks[i] != chk && chk.checked) {
-            chks[i].checked = false;
+    var checkBoxes = document.getElementsByClassName("correct-answer-checkbox");
+    for (var i = 0, len = checkBoxes.length; i < len; i++) {
+        if (checkBoxes[i] != el) {
+            console.log(checkBoxes[i]);
+            checkBoxes[i].checked = false;
         }
     }
-}
+};
+
+
+
+
+//function check(el) {
+//    var checkBoxes = document.getElementsByClassName("correct-answer-checkbox");
+//    checkBoxes.forEach(function (item) {
+//        if (item != el) {
+//            console.log(item);
+//            item.checked = false;
+//        }
+//    });
+//}
