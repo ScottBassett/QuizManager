b78556ee7f5d65c8c2a708047cce59bbe5c5ffe7
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2021-01-18 16:15:47 +0000

    Got the Edit Question and Answer page working.

---
+++ QuizManager.Web/ViewModels/EditQuestionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using QuizManager.DataAccess.Models;
+
+namespace QuizManager.Web.ViewModels
+{
+    public class EditQuestionViewModel
+    {
+        public Question Question { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
--- QuizManager.Web/wwwroot/js/site.js
+++ QuizManager.Web/wwwroot/js/site.js
@@ -3,7 +3,6 @@ function check(el) {
     var checkBoxes = document.getElementsByClassName("correct-answer-checkbox");
     for (var i = 0, len = checkBoxes.length; i < len; i++) {
         if (checkBoxes[i] != el) {
-            console.log(checkBoxes[i]);
             checkBoxes[i].checked = false;
         }
     }
