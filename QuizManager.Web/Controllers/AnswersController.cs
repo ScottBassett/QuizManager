787f9db5d5f57632360cc8f274dc80aa89f7891f
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2021-01-22 10:06:22 +0000

    Tidying routing.

--- QuizManager.Web/Controllers/AnswersController.cs
+++ QuizManager.Web/Controllers/AnswersController.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using QuizManager.DataAccess;
-using QuizManager.DataAccess.Models;
 using QuizManager.Web.ViewModels;
 
 namespace QuizManager.Web.Controllers
@@ -25,24 +22,6 @@ namespace QuizManager.Web.Controllers
             return View(await _context.Answers.ToListAsync());
         }
 
-        // GET: Answers/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var answer = await _context.Answers
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (answer == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(answer);
-        //}
-
         // GET: Answers/Create
         public IActionResult Create()
         {
@@ -50,110 +29,25 @@ namespace QuizManager.Web.Controllers
             return View();
         }
 
-        // POST: Answers/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create(CreateAnswersViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        foreach (var answer in model.Answers)
-        //        {
-        //            answer.QuestionId = model.QuestionId;
-        //        }
+       // POST: Answers/Create
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateAnswersViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var answer in model.Answers)
+                {
+                    answer.QuestionId = model.QuestionId;
+                }
 
-        //        await _context.AddRangeAsync(model.Answers);
-        //        await _context.SaveChangesAsync();
+                await _context.AddRangeAsync(model.Answers);
+                await _context.SaveChangesAsync();
 
-        //        return RedirectToAction("Index", "Quizzes");
-        //    }
-            
-        //    return View(model);
-        //}
+                return RedirectToAction("Index", "Quizzes");
+            }
 
-        //// GET: Answers/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var answer = await _context.Answers.FindAsync(id);
-        //    if (answer == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    ViewData["Question"] = new SelectList(_context.Questions, "Id", "QuestionName", TempData["questionId"]);
-        //    return View(answer);
-        //}
-
-        //// POST: Answers/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, CreateAnswersViewModel model)
-        //{
-        //    if (id != answer.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(answer);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!AnswerExists(answer.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(model);
-        //}
-
-        // GET: Answers/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var answer = await _context.Answers
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (answer == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(answer);
-        ////}
-
-        //// POST: Answers/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var answer = await _context.Answers.FindAsync(id);
-        //    _context.Answers.Remove(answer);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        //private bool AnswerExists(int id)
-        //{
-        //    return _context.Answers.Any(e => e.Id == id);
-        //}
+            return View(model);
+        }
     }
 }
